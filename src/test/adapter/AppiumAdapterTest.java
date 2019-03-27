e97bc1c5a8b4d21cfeaacffb0185e2e3acf25377
YuXin Chen <likesm0857@gmail.com>
2019-03-27 19:57:50 +0800

    upload build code-CoverageScript

---
+++ src/test/adapter/AppiumAdapterTest.java
@@ -0,0 +1,26 @@
+package adapter;
+
+import config.ConfigReader;
+import entity.xPath.NodeAttribute;
+import entity.xPath.XPathBuilder;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidKeyCode;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+import static org.junit.Assert.*;
+
+public class AppiumAdapterTest {
+    private AndroidDriver driver;
+
+    @Test
+    public void launchAppTest() throws IOException, InterruptedException {
+
+
+
+    }
+}
