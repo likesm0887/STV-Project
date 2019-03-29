9f0409901aebee666b6a5a987c41f8968058b6f9
YuXin Chen <likesm0857@gmail.com>
2019-03-29 19:01:32 +0800

    refactor

--- src/test/ScriptParserTest.java
+++ src/test/ScriptParserTest.java
@@ -1,34 +1,55 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
-
 public class ScriptParserTest {
     private ScriptParser scriptParser;
     private List<Instruction> instructions;
     @Before
-    public void SetUp()
-    {
+    public void SetUp() {
         scriptParser = new ScriptParser();
 
     }
+
     @Test
     public void parse() throws Exception {
+    /*
+    MainActivity	Click	TASK_LABEL{5}
 
+    MainActivity	Type_Text{45 6@}	SETTING_BUTTON{1}
 
-        instructions = scriptParser.parse(System.getProperty("user.dir")+"\\scriptForTest.txt");
+    MainActivity	Click	TASK_LABEL
 
-        Assert.assertEquals(instructions.get(0).getActivity(),"MainActivity");
-        Assert.assertEquals(instructions.get(0).getEvent(),"Click");
-        Assert.assertEquals(instructions.get(0).getAttribute(),"TASK_LABEL");
-        Assert.assertEquals(instructions.get(1).getActivity(),"MainActivity");
-        Assert.assertEquals(instructions.get(1).getEvent(),"Click");
-        Assert.assertEquals(instructions.get(1).getAttribute(),"SETTING_BUTTON");
-        Assert.assertEquals(instructions.get(1).getParameter(),"1");
-        Assert.assertEquals(instructions.size(),8);
+    MainActivity	Click	SETTING_BUTTON
+
+    MainActivity	Type_Text{0}	SETTING_BUTTON
+
+    MainActivity	Click	SETTING_BUTTON{7}
+
+    MainActivity	Click	TASK_LABEL
+    MainActivity	Click	SETTING_BUTTON 5
+     */
+
+        instructions = scriptParser.parse(System.getProperty("user.dir") + "\\scriptForTest.txt");
+        Assert.assertEquals(instructions.get(0).getActivity(), "MainActivity");
+        Assert.assertEquals(instructions.get(0).getEvent(), "Click");
+        Assert.assertEquals(instructions.get(0).getAttribute(), "TASK_LABEL");
+        Assert.assertEquals(instructions.get(0).getEventParameter(), Optional.empty());
+        Assert.assertEquals(instructions.get(0).getElementParameter().get(), "5");
+        Assert.assertEquals(instructions.get(1).getActivity(), "MainActivity");
+        Assert.assertEquals(instructions.get(1).getEvent(), "Type_Text");
+        Assert.assertEquals(instructions.get(1).getEventParameter().get(), "45 6@");
+        Assert.assertEquals(instructions.get(1).getAttribute(), "SETTING_BUTTON");
+        Assert.assertEquals(instructions.size(), 8);
     }
 
     @Test(expected = Exception.class)
@@ -38,5 +59,23 @@ public class ScriptParserTest {
         } catch (Exception e) {
             throw new Exception("系統找不到檔案");
         }
+    }
+
+    @Test
+    public void rexTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = ScriptParser.class.getDeclaredMethod("rex", String.class);
+        method.setAccessible(true);
+        String  testString ="Type_Text{45 6@}";
+        Object[] arguments = new Object[]{testString};
+        assertEquals("45 6@",  method.invoke(scriptParser,arguments));
+        testString= "SETTING_BUTTON{1}";
+        arguments = new Object[]{testString};
+        assertEquals("1", method.invoke(scriptParser,arguments));
+        testString= "SETTING_BUTTON";
+        arguments = new Object[]{testString};
+        assertEquals(null,  method.invoke(scriptParser,arguments));
+//todo invoke
+        assertEquals("Type_Text", scriptParser.rexFilter("Type_Text{45 6@}"));
+        assertEquals("Type_Text", scriptParser.rexFilter("Type_Text"));
     }
 }
