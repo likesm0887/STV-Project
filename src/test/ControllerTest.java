570bafa621b49e278098bfad5d75a73224a31a93
Jammy Tan <jammy1129@gmail.com>
2019-04-01 11:10:13 +0800

    xpath test

--- src/test/ControllerTest.java
+++ src/test/ControllerTest.java
@@ -164,4 +164,185 @@ public class ControllerTest {
         element.swipe(SwipeElementDirection.LEFT, 100, 100, 1000);
         driver.rotate(ScreenOrientation.LANDSCAPE);
     }
+
+    @Test
+    public void settingTest(){
+//        XPathBuilder builder = new XPathBuilder();
+//        builder.append("android.support.v7.widget.LinearLayoutCompat")
+//                .at(NodeAttribute.INDEX, "1");
+//        adapter.clickElement(builder.toString());
+        MobileElement elementTwo = (MobileElement) driver.findElement(By.xpath("//*[@class='android.support.v7.widget.LinearLayoutCompat']"));
+        elementTwo.click();
+    }
+
+    @Test
+    public void folderListTest(){
+//        XPathBuilder builder = new XPathBuilder();
+//        builder.append("android.widget.RelativeLayout")
+//                .at(NodeAttribute.INDEX, "0");
+//        adapter.clickElement(builder.toString());
+//        MobileElement element = (MobileElement) driver.findElement(By.xpath("//*[@class='android.widget.RelativeLayout' and @index='0']"));
+        MobileElement element = (MobileElement) driver.findElement(By.xpath("//*[@text='My tasks']"));
+        element.click();
+    }
+
+    @Test
+    public void folderListWithTaskTest(){
+        MobileElement folder = (MobileElement) driver.findElement(By.xpath("//*[@text='My tasks']"));
+        folder.click();
+
+        // add 1st task
+        MobileElement quickAdd = (MobileElement) driver.findElement(By.xpath("//*[@index='0']/android.widget.ImageView[@resource-id='org.dmfs.tasks:id/quick_add_task']"));
+        quickAdd.click();
+
+        adapter.waitFor(1000);
+
+
+        MobileElement taskName = (MobileElement) driver.findElement(By.xpath("//*[@resource-id='android:id/input']"));
+        taskName.sendKeys("quick add");
+
+        MobileElement addTask = (MobileElement) driver.findElement(By.xpath("//*[@resource-id='android:id/button1']"));
+        addTask.click();
+
+        adapter.waitFor(1000);
+
+        // add 2nd task
+        MobileElement quickAdd1 = (MobileElement) driver.findElement(By.xpath("//*[@index='0']/android.widget.ImageView[@resource-id='org.dmfs.tasks:id/quick_add_task']"));
+        quickAdd1.click();
+
+        adapter.waitFor(1000);
+        taskName = (MobileElement) driver.findElement(By.xpath("//*[@resource-id='android:id/input']"));
+        taskName.sendKeys("quick add2");
+        addTask = (MobileElement) driver.findElement(By.xpath("//*[@resource-id='android:id/button1']"));
+        addTask.click();
+        adapter.waitFor(1000);
+
+        // click task
+        MobileElement task = (MobileElement) driver.findElement(By.xpath("//*[@text='quick add']"));
+        task.click();
+
+    }
+
+    @Test
+    public void tabTest() {
+        MobileElement elementTwo = (MobileElement) driver.findElement(By.xpath("//*[@class='android.support.v7.app.ActionBar$Tab' and @index='0']"));
+        elementTwo.click();
+    }
+
+    @Test
+    public void floatAddTaskTest() {
+        MobileElement elementTwo = (MobileElement) driver.findElement(By.xpath("//*[@class='android.widget.ImageButton']"));
+        elementTwo.click();
+    }
+
+    @Test
+    public void quickAddTest(){
+        // select folder
+        MobileElement folderList = (MobileElement) driver.findElement(By.xpath("//*[@class='android.widget.RelativeLayout' and @index='0']"));
+        folderList.click();
+        // click quick add
+        MobileElement quickAdd = (MobileElement) driver.findElement(By.xpath("//*[@index='0']/android.widget.ImageView[@resource-id='org.dmfs.tasks:id/quick_add_task']"));
+        quickAdd.click();
+        // edit button
+//        MobileElement editBtn = (MobileElement) driver.findElement(By.xpath("//*[@resource-id='android:id/edit']"));
+//        editBtn.click();
+        // spinner(select folder)
+        MobileElement spinner = (MobileElement) driver.findElement(By.xpath("//*[@resource-id='org.dmfs.tasks:id/task_list_spinner']"));
+        spinner.click();
+        // spinner options
+        MobileElement options = (MobileElement) driver.findElement(By.xpath("//*[@index='0' and @class='android.widget.LinearLayout']"));
+        options.click();
+        // edit text
+        MobileElement editText = (MobileElement) driver.findElement(By.xpath("//*[@resource-id='android:id/input']"));
+        editText.sendKeys("Test");
+        // save and continue
+        MobileElement saveAndContinueBtn = (MobileElement) driver.findElement(By.xpath("//*[@resource-id='android:id/button2']"));
+        saveAndContinueBtn.click();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // save
+        MobileElement saveBtn = (MobileElement) driver.findElement(By.xpath("//*[@resource-id='android:id/button1']"));
+        saveBtn.click();
+    }
+
+    @Test
+    public void createTaskTest(){
+        // create task
+        MobileElement createTask = (MobileElement) driver.findElement(By.xpath("//*[@class='android.widget.ImageButton']"));
+        createTask.click();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // spinner
+        MobileElement spinner = (MobileElement) driver.findElement(By.xpath("//*[@resource-id='org.dmfs.tasks:id/task_list_spinner']"));
+        spinner.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // options
+        MobileElement options = (MobileElement) driver.findElement(By.xpath("//*[@index='0' and @class='android.widget.LinearLayout']"));
+        options.click();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // input title
+//        MobileElement editTitle = (MobileElement) driver.findElement(By.xpath("//*[@index='0']/android.widget.EditText"));
+        MobileElement editTitle = (MobileElement) driver.findElement(By.xpath("//*[@index='0' and contains(@class, 'android.widget.EditText')]"));
+        editTitle.sendKeys("Test");
+        // select status
+        MobileElement status = (MobileElement) driver.findElement(By.xpath("//*[@index='1' and contains(@class, 'android.widget.Spinner')]"));
+        status.click();
+        // status options
+//        MobileElement statusOptions = (MobileElement) driver.findElement(By.xpath("//*[@index='1' and @class='android.widget.LinearLayout']"));
+        MobileElement statusOptions = (MobileElement) driver.findElement(By.xpath("//*[@text='in process']"));
+        statusOptions.click();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // save
+        MobileElement saveBtn = (MobileElement) driver.findElement(By.xpath("//*[@resource-id='org.dmfs.tasks:id/editor_action_save']"));
+        saveBtn.click();
+    }
+
+    @Test
+    public void inputValueTest(){
+        // create task
+        MobileElement createTask = (MobileElement) driver.findElement(By.xpath("//*[@class='android.widget.ImageButton']"));
+        createTask.click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // location
+        MobileElement editLocation = (MobileElement) driver.findElement(By.xpath("//*[@index='2' and contains(@class, 'android.widget.EditText')]"));
+
+        editLocation.sendKeys("Test location");
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        XPathBuilder builder = new XPathBuilder();
+        builder.append("android.widget.ExpandableListView")
+                .at(NodeAttribute.RESOURCE_ID, "android:id/list");
+        MobileElement element = adapter.findElement(builder.toString());
+        element.swipe(SwipeElementDirection.UP, 100, 100, 1000);
+
+        // description
+        MobileElement editDescription = (MobileElement) driver.findElement(By.xpath("//*[@index='3' and contains(@class, 'android.widget.EditText')]"));
+        editDescription.sendKeys("Test description");
+    }
 }
