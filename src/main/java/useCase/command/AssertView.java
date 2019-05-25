e29243919364d540f9a9fa36aa6375edd48260e6
YuXin Chen <likesm0857@gmail.com>
2019-05-25 16:52:46 +0800

    rename assertActivity to assertView And add View to Activtity

--- src/main/java/useCase/command/AssertView.java
+++ src/main/java/useCase/command/AssertView.java
@@ -3,13 +3,17 @@ package useCase.command;
 import adapter.device.DeviceDriver;
 
 public class AssertActivityCommand extends Command {
-    private String exceptActivity ;
-    public AssertActivityCommand(DeviceDriver deviceDriver,String exceptActivity) {
+    private String exceptActivity;
+
+    public AssertActivityCommand(DeviceDriver deviceDriver, String exceptActivity) {
         super(deviceDriver, "");
         this.exceptActivity = exceptActivity;
     }
+
     @Override
     public void execute() {
         deviceDriver.assertActivity(exceptActivity);
     }
+
+
 }
