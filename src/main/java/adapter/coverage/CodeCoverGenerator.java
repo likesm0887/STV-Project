4c8b09fbdbde25ed354da39ea019c22b3636515d
YuXin Chen <likesm0857@gmail.com>
2019-06-07 16:49:25 +0800

    fix bug

--- src/main/java/adapter/coverage/CodeCoverGenerator.java
+++ src/main/java/adapter/coverage/CodeCoverGenerator.java
@@ -8,15 +8,16 @@ import java.nio.file.Paths;
 public class CodeCoverGenerator {
     private final String PACKAGE_NAME = "org.dmfs.tasks";
     private final String ADB_PATH = Paths.get(System.getenv("ANDROID_HOME"), "platform-tools", "adb").toString();
+    private final String COVERAGE_PATH = System.getProperty("user.dir") + "/" + "/codeCoverage";
     private int coverageCounter = 0;
     private Config config;
 
     public CodeCoverGenerator(Config config) {
         this.config = config;
+        initCodeCoverageFolder(COVERAGE_PATH);
     }
 
     public void pullCodeCoverage() {
-        final String COVERAGE_PATH = System.getProperty("user.dir") + "/" + "/codeCoverage";
         new File(COVERAGE_PATH).mkdirs();
         File coverageFile = new File(COVERAGE_PATH + "/coverage" + this.coverageCounter + ".ec");
         String[] pullToLocalCmd = {ADB_PATH, "exec-out", "run-as", PACKAGE_NAME, "cat", "/data/data/" + PACKAGE_NAME + "/files/coverage.ec"};
@@ -30,6 +31,19 @@ public class CodeCoverGenerator {
         }
         if (checkFileCanUse(coverageFile)) {
             this.coverageCounter += 1;
+        }
+    }
+
+    private void initCodeCoverageFolder(String path) {
+        File file = new File(path);
+        if (!file.exists()) {//判断是否待删除目录是否存在
+            throw  new RuntimeException("coverage folder is not Exist");
+        }
+        String[] content = file.list();//取得当前目录下所有文件和文件夹
+        for (String name : content) {
+            File temp = new File(path, name);
+            temp.delete();
+
         }
     }
 
